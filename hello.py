7d4b118f324d68678f0642da8ec48720fd20f166
Osaid Zaher <zaher.osaid@gmail.com>
2025-09-26 10:27:33 +0100

    adding new route /contact

--- hello.py
+++ hello.py
@@ -1,11 +1,14 @@
 from flask import Flask
-
-
 app = Flask(__name__)
 
-
 @app.route('/')
-
 def say_hello():
+    return '<p>Hello, World, I am a Flask app!</p><p><a href="/about">About this app</a></p><p><a href="/contact">Contact</a></p>'
 
-return '<p>Hello, World, I am a Flask app!</p>'
+@app.route('/about')
+def about():
+    return '<p>This application is running on the Flask web framework.</p><p>Learn more about Flask at <a href="https://flask.palletsprojects.com/">https://flask.palletsprojects.com/</a></p><p><a href="/">Back to home</a></p>'
+
+@app.route('/contact')
+def contact():
+    return '<p>Contact me at: your.email@example.com</p><p><a href="/">Back to home</a></p>'
